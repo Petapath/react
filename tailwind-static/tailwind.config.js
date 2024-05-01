84a4b5c58de888e47bb4fa3644ec6cf19fd2804e
Michal Harasimiuk <michal@Michals-MacBook-Pro.local>
2024-05-01 23:57:02 +0100

    Static website with make system.

---
+++ tailwind-static/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary:   '#F3C969',
+        secondary: '#EDFF86'
+      },
+    },
+  },
+  plugins: [],
+}
+

6559e22578250605ec99f84e602d36e691b30c0b
preetanandvakt <preet.anand@vakt.io>
2024-05-23 22:07:26 +0100

    [Preet] add routing to hello-world

---
+++ hello-world/src/AppWithRouter.js
@@ -0,0 +1,24 @@
+import './output.css';
+
+import {Route, Router, Switch} from 'react-router';
+import {createBrowserHistory} from 'history';
+
+import Page1 from './page1.jsx';
+import Page2 from './page2.jsx';
+
+const history = createBrowserHistory();
+
+const AppWithRouter = (() => {
+
+    return (
+        <Router history={history}>
+            <Switch>
+                <Route exact path='/' render={() => <Page1/>}/>
+                <Route exact path='/page1' render={() => <Page1/>}/>
+                <Route exact path='/page2' render={() => <Page2/>}/>
+            </Switch>
+        </Router>
+    )
+});
+
+export default AppWithRouter;
